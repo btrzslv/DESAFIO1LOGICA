fac663a805e10690490469a79fcdd507b4e3f2d0
Beatriz <143760674+btrzslv@users.noreply.github.com>
2023-12-08 00:31:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let mensagem = "Ano novo está chegando!!";
+console.log(mensagem);
+
+let falsete = "Mariah Carey";
+console.log(falsete);
+
+let idadeFalsete = "54";
+console.log(idadeFalsete);
+
+let resultado = 2023 - 54;
+console.log(resultado);
+
+let nascimento = 1969;
+console.log(nascimento);
+
+let quartaFeira = false;
+console.log(quartaFeira);
+
+let maiorDeDezoito =
+  "Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.";
+console.log(maiorDeDezoito);
+
+if (maiorDeDezoito >= 18) {
+  console.log(
+    "Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey."
+  );
+} else {
+  console.log("Sinto muito, mas assistirá da tv globo.");
+}
+
+let mês = "agosto || dezembro || junho";
+console.log(mês);
+
+if ((mês = "Agosto || Dezembro || Junho")) {
+  console.log(
+    "Uma ou mais opções estão corretas. O mês escolhido foi o mês de Agosto."
+  );
+} else {
+  console.log(
+    "Algo de errado não está certo, o mês digitado foi o mês de Dezembro."
+  );
+}
